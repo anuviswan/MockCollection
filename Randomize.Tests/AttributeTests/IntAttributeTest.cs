3875be2a749bee6e9fe4b81981a14b379137e079
Anu Viswan <anu.viswan@gmail.com>
2019-01-26 15:42:34 +0530

    Adding more unit tests

---
+++ Randomize.Tests/AttributeTests/IntAttributeTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Randomize.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockCollecton.Tests.AttributeTests
+{
+    [TestClass]
+    public class IntAttributeTest
+    {
+        [TestMethod]
+        public void ValidLimit_ValuesRemainsWithinRange()
+        {
+            var random = new Random();
+            var dataList = random.GenerateCollection<Data>(100);
+            Assert.IsTrue(dataList.All(x => x.Number > 10 && x.Number < 20));
+        }
+
+        private class Data
+        {
+            [Randomize.Net.Attributes.Int32.Limit(Max = 20, Min = 10)]
+            public double Number { get; set; }
+        }
+    }
+}
---
+++ Randomize/Attributes/Int32/LimitAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Randomize.Net.Attributes.Int32
+{
+    [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    public sealed class LimitAttribute : Attribute, ILimit<int>
+    {
+        public int Max { get; set; }
+        public int Min { get; set; }
+    }
+}
