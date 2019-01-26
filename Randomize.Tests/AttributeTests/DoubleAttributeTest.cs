b28aea139d071b7ed117ec1f9abe1cacfe050458
Anu Viswan <anu.viswan@gmail.com>
2019-01-26 11:36:21 +0530

    Adding Test Case for DoubleAttribute

---
+++ Randomize.Tests/AttributeTests/DoubleAttributeTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Randomize.Net;
+using System;
+using System.Linq;
+
+namespace MockCollecton.Tests.AttributeTests
+{
+    [TestClass]
+    public class DoubleAttributeTest
+    {
+        [TestMethod]
+        public void ValidLimit_ValuesRemainsWithinRange()
+        {
+            var random = new Random();
+            var dataList = random.GenerateCollection<Data>(100);
+            Assert.IsTrue(dataList.All(x => x.Number > 10 && x.Number < 20));
+        }
+
+        private class Data
+        {
+            [Randomize.Net.Attributes.Double.Limit(Max =20, Min =10)]
+            public double Number { get; set; }
+        }
+    }
+}
